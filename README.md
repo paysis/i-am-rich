02fcb3dd46339503bd0bb2a1298edf6effb690d1
paysis <paysis21@gmail.com>
2023-06-21 14:17:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# I am rich
+
+A project created for educational purposes.
---
+++ android/app/src/main/kotlin/unique/com/paysis/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package unique.com.paysis.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
